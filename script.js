732afc3d62d9ca3080f6f4708376f6aa372d7196
Leo McArdle <leomcardle@gmail.com>
2020-04-11 12:17:17 +0100

    post-prettier

--- script.js
+++ script.js
@@ -1,66 +1,67 @@
-import Client from "./lib/Client.js"
-import Server from "./lib/Server.js"
+import Client from "./lib/Client.js";
+import Server from "./lib/Server.js";
 
-const video = document.querySelector("video")
-const files = document.querySelector("input[type=file]")
+const video = document.querySelector("video");
+const files = document.querySelector("input[type=file]");
 
-if (getParameterByName('d')) {
+if (getParameterByName("d")) {
   // we're a client
 
-  $('body').addClass('client')
+  $("body").addClass("client");
 
   new Client({
-    data_string: getParameterByName('d'),
+    data_string: getParameterByName("d"),
     on_channel_open: (e) => {
-      console.log('on_channel_open')
-      video.srcObject = e.streams[0]
-      video.play()
+      console.log("on_channel_open");
+      video.srcObject = e.streams[0];
+      video.play();
     },
-  })
-
+  });
 } else {
   // we're a server
 
-  ['signalling', 'server', 'username', 'password'].forEach(x => {
-    $('#' + x).val(localStorage.getItem(x))
-  })
+  ["signalling", "server", "username", "password"].forEach((x) => {
+    $("#" + x).val(localStorage.getItem(x));
+  });
 
   $("#save").click(() => {
-    $('#settings_modal').modal('hide')
-    ;['signalling', 'server', 'username', 'password'].forEach(x => {
-      localStorage.setItem(x, $('#' + x).val())
-    })
-  })
+    $("#settings_modal").modal("hide");
+    ["signalling", "server", "username", "password"].forEach((x) => {
+      localStorage.setItem(x, $("#" + x).val());
+    });
+  });
 
   function updateVideo() {
-    const file = files.files[0]
-    video.src = URL.createObjectURL(file)
-    const stream = video.captureStream ? video.captureStream() : video.mozCaptureStream()
+    const file = files.files[0];
+    video.src = URL.createObjectURL(file);
+    const stream = video.captureStream
+      ? video.captureStream()
+      : video.mozCaptureStream();
     new Server({
-      signalling_url: localStorage.getItem('signalling'),
+      signalling_url: localStorage.getItem("signalling"),
       peer_config: {
         iceServers: [
           {
             urls: document.querySelector("#server").value,
             username: document.querySelector("#username").value,
-            credential: document.querySelector("#password").value
-          }
-        ]
+            credential: document.querySelector("#password").value,
+          },
+        ],
       },
       stream,
       on_share: (data_string) => {
-        $("#share_url").val("https://stream.mcardle.io/?d=" + data_string)
-        $("#share_modal").modal('show')
-      }
-    })
+        $("#share_url").val("https://stream.mcardle.io/?d=" + data_string);
+        $("#share_modal").modal("show");
+      },
+    });
   }
 
-  files.addEventListener("input", e => {
-    updateVideo()
-  })
+  files.addEventListener("input", (e) => {
+    updateVideo();
+  });
 }
 
 function getParameterByName(name) {
-  var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search)
-  return match && decodeURIComponent(match[1].replace(/\+/g, ' '))
+  var match = RegExp("[?&]" + name + "=([^&]*)").exec(window.location.search);
+  return match && decodeURIComponent(match[1].replace(/\+/g, " "));
 }
--- server.js
+++ server.js
@@ -1,32 +1,38 @@
-var uuid = require('uuid')
-var WebSocketServer = require('ws').Server;
+var uuid = require("uuid");
+var WebSocketServer = require("ws").Server;
 
 var wss = new WebSocketServer({ port: 8081, clientTracking: true });
 
-wss.on('connection', (ws) => {
-  ws.id = uuid.v4()
-  ws.on('message', (dataString) => {
-    var data = JSON.parse(dataString)
-    console.log(data)
+wss.on("connection", (ws) => {
+  ws.id = uuid.v4();
+  ws.on("message", (dataString) => {
+    var data = JSON.parse(dataString);
+    console.log(data);
     try {
-      var client = Array.from(wss.clients).find(e => e.id == data.to)
+      var client = Array.from(wss.clients).find((e) => e.id == data.to);
       if (client) {
-        client.send(JSON.stringify({
-          ...data,
-          from: ws.id
-        }))
+        client.send(
+          JSON.stringify({
+            ...data,
+            from: ws.id,
+          })
+        );
       } else {
-        ws.send(JSON.stringify({
-          type: 'lost-peer'
-        }))
+        ws.send(
+          JSON.stringify({
+            type: "lost-peer",
+          })
+        );
       }
-    } catch(e){
-      console.log(e)
+    } catch (e) {
+      console.log(e);
     }
-  })
+  });
 
-  ws.send(JSON.stringify({
-    type: 'hello',
-    id: ws.id
-  }))
-})
+  ws.send(
+    JSON.stringify({
+      type: "hello",
+      id: ws.id,
+    })
+  );
+});
